5db5c36d2df52b5eac8b29ba054b406589fe803c
ABITO <hancom114@gmail.com>
2023-03-16 00:39:06 +0900

    optimized for nextjs

--- components/sectionPartners/partners.js
+++ components/sectionPartners/partners.js
@@ -6,52 +6,52 @@ export default function Partners() {
 
     return (
         <>
-          <section id="partners" class="text-gray-600 body-font">
+          <section id="partners" className="text-gray-600 body-font">
 
-            <div class="container px-5 py-24 mx-auto">
+            <div className="container px-5 py-24 mx-auto">
               <div className="flex flex-col text-center w-full mb-20">
                       <h1 className="font-BrothersBold sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Partners</h1>
                       {/* <p className="font-NotoSansBold lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> */}
               </div>
-              <div class="flex flex-wrap -m-4">
-                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                  <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></img>
+              <div className="flex flex-wrap -m-4">
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <a className="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></Image>
                   </a>
-                  {/* <div class="mt-4">
-                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                    <p class="mt-1">$16.00</p>
+                  {/* <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p className="mt-1">$16.00</p>
                   </div> */}
                 </div>
-                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                  <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></img>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <a className="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></Image>
                   </a>
-                  {/* <div class="mt-4">
-                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                    <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                    <p class="mt-1">$21.15</p>
+                  {/* <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                    <p className="mt-1">$21.15</p>
                   </div> */}
                 </div>
-                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                  <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></img>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <a className="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></Image>
                   </a>
-                  {/* <div class="mt-4">
-                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                    <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                    <p class="mt-1">$12.00</p>
+                  {/* <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                    <p className="mt-1">$12.00</p>
                   </div> */}
                 </div>
-                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                  <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></img>
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <a className="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x420"></Image>
                   </a>
-                  {/* <div class="mt-4">
-                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                    <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                    <p class="mt-1">$18.40</p>
+                  {/* <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                    <p className="mt-1">$18.40</p>
                   </div> */}
                 </div>
                 </div>
--- components/sectionSolutions/solutions.js
+++ components/sectionSolutions/solutions.js
@@ -57,45 +57,49 @@ export default function Solutions() {
         <>
             <section id="solutions" className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto flex flex-wrap flex-col">
+                <div className="flex flex-col text-center w-full mb-20">
+                    <h1 className="font-BrothersBold sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Solution</h1>
+                    {/* <p className="font-NotoSansBold lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> */}
+                    </div>
                 <div className="flex mx-auto flex-wrap mb-20">
-                <a class="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-indigo-500 text-indigo-500 tracking-wider rounded-t"
+                <a className="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-indigo-500 text-indigo-500 tracking-wider rounded-t"
                     onClick={() => 
                       {
                         setSolution("deploy");
                       }
                     }>
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
                         <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                     </svg>DEPLOY
                 </a>
-                <a class="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
+                <a className="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
                     onClick={() => 
                       {
                         setSolution("promote");
                       }
                     }>
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
                         <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                     </svg>PROMOTE
                 </a>
-                <a class="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
+                <a className="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
                     onClick={() => 
                       {
                         setSolution("build");
                       }
                     }>
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
                         <circle cx="12" cy="5" r="3"></circle>
                         <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
                     </svg>BUILD
                 </a>
-                <a class="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
+                <a className="font-NotoSans sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
                     onClick={() => 
                       {
                         setSolution("play");
                       }
                     }>
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
                     </svg>PLAY
@@ -107,10 +111,10 @@ export default function Solutions() {
                   {solution === 'build' && <BuildAnimation/>}
                   {solution === 'play' && <PlayAnimation/>}
                 </div>
-                {/* <img class="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 block mx-auto mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"></img> */}
-                <div class="flex flex-col text-center w-full">
-                <h1 class="font-NotoSansBold  text-xl font-medium title-font mb-4 text-gray-900">{title}</h1>
-                <p class="font-NotoSans text-center lg:w-1/4  mx-auto leading-relaxed">{description}</p>
+                {/* <img className="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 block mx-auto mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"></img> */}
+                <div className="flex flex-col text-center w-full">
+                <h1 className="font-NotoSansBold  text-xl font-medium title-font mb-4 text-gray-900">{title}</h1>
+                <p className="font-NotoSans text-center lg:w-1/4  mx-auto leading-relaxed">{description}</p>
                 </div>
             </div>
             </section>
